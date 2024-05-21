1395c6188ec6ec715ea6225340838ef289faf00f
Augusto-Carissimo <augusto.carissimo@gmail.com>
2024-05-21 18:42:42 -0300

    Refactor ExpensesController

--- expenses_tracker_be/app/controllers/expenses_controller.rb
+++ expenses_tracker_be/app/controllers/expenses_controller.rb
@@ -1,14 +1,30 @@
 class ExpensesController < ApplicationController
+  skip_before_action :verify_authenticity_token
   rescue_from StandardError, with: :handle_conversion_error
+
+  def index
+    @last_expenses = LastExpenses.new.call
+    render json: @last_expenses
+  end
 
   def create
     CreateExpense.new(params: expense_params, flash: flash).call
-    redirect_to root_path
+    render json: { message: 'Creating Expense' }, status: :ok
   end
 
   def destroy
     DestroyExpense.new(params: set_expense, flash: flash).call
-    redirect_to root_path
+    render json: { message: 'Deleting Expense' }, status: :ok
+  end
+
+  def total_expenses
+    total = CalculateExpenses.new.calculate_total
+    render json: { total_expenses: total }
+  end
+
+  def monthly_expenses
+    monthly = CalculateExpenses.new.calculate_monthly
+    render json: { monthly_expenses: monthly }
   end
 
   private
@@ -24,6 +40,6 @@ class ExpensesController < ApplicationController
   def handle_conversion_error(exception)
     Rails.logger.error "An error occurred: #{exception.message}"
     flash[:alert] = 'Failed to convert cost to dolars. Expense was not added.'
-    redirect_to root_path
+    render json: { message: 'Error' }, status: :ok
   end
 end
