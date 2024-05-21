517705f8006a8b3d3c7bc4a6ede9feffacba3603
Augusto-Carissimo <augusto.carissimo@gmail.com>
2024-05-21 18:27:07 -0300

    Remove HomeController

--- expenses_tracker_be/config/routes.rb
+++ expenses_tracker_be/config/routes.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root "home#index"
   resources :types
   resources :expenses, except: [:show]
-  get 'home/test'
   get 'expenses/total_expenses', to: 'expenses#total_expenses'
   get 'expenses/monthly_expenses', to: 'expenses#monthly_expenses'
 end
