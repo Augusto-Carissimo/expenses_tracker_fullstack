c4cf9a00aab7137b1eaafdf21790fc8b908785a4
Augusto-Carissimo <augusto.carissimo@gmail.com>
2024-05-21 16:54:08 -0300

    Add total and monthly expenses

--- expenses_tracker_be/config/routes.rb
+++ expenses_tracker_be/config/routes.rb
@@ -3,6 +3,8 @@
 Rails.application.routes.draw do
   root "home#index"
   resources :types
-  resources :expenses
+  resources :expenses, except: [:show]
   get 'home/test'
+  get 'expenses/total_expenses', to: 'expenses#total_expenses'
+  get 'expenses/monthly_expenses', to: 'expenses#monthly_expenses'
 end
