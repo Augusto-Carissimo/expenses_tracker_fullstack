2baa6e5ce871addf25c62205deac85662d6d2eb9
Augusto-Carissimo <augusto.carissimo@gmail.com>
2024-05-24 12:19:29 -0300

    Remove dolar convertion

--- expenses_tracker_be/app/use_cases/create_expense.rb
+++ expenses_tracker_be/app/use_cases/create_expense.rb
@@ -7,15 +7,7 @@ class CreateExpense
 
   def call
     expense = Expense.new(@params)
-    convert_cost_to_dolars(expense) if expense.cost.present?
     @flash[:notice] = 'New expense added successfully.' if expense.save
     @flash.now[:alert] = 'Failed to add the expense. Please check the form.' unless expense.persisted?
-  end
-
-  private
-
-  def convert_cost_to_dolars(expense)
-    dolar_price = DolarService.new.fetch_dolar_price
-    expense.cost /= dolar_price
   end
 end
